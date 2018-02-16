2a2637842d2bda47bc52ab50badeb96128abaaf0
David Bonnet <david@bonnet.cc>
2018-02-16 01:24:17 +0100

    Update `withFocus` to support uncontrolled mode

--- src/input.js
+++ src/input.js
@@ -2,26 +2,34 @@ import { compose, branch, withHandlers, mapProps } from 'recompose'
 
 import { onPropsChange, hasProps, withBuffer } from './tools'
 
-export const withFocus = compose(
-  branch(
-    hasProps(['focus', 'node', 'onChangeFocus']),
-    compose(
-      withBuffer(({ focus }) => !focus),
-      withHandlers({
-        onFocus: ({ buffer, setBuffer, onChangeFocus }) => () =>
-          buffer ? undefined : setBuffer(true, () => onChangeFocus(true)),
-        onBlur: ({ buffer, setBuffer, onChangeFocus }) => () =>
-          !buffer ? undefined : setBuffer(false, () => onChangeFocus(false)),
-      }),
-      onPropsChange(['node', 'focus'], props => {
-        const { node, focus, buffer } = props
-        if (node == null || focus === buffer) {
-          return
-        }
-        node[focus ? 'focus' : 'blur']()
-      }),
-      mapProps(props => withBuffer.omit(props)),
-    ),
+export const withFocus = branch(
+  hasProps(['node']),
+  compose(
+    withBuffer(({ focused }) => (focused == null ? false : !focused)),
+    withHandlers({
+      onFocus: ({ buffer, setBuffer, onChangeFocus }) => () =>
+        buffer
+          ? undefined
+          : setBuffer(
+              true,
+              onChangeFocus == null ? undefined : () => onChangeFocus(true),
+            ),
+      onBlur: ({ buffer, setBuffer, onChangeFocus }) => () =>
+        !buffer
+          ? undefined
+          : setBuffer(
+              false,
+              onChangeFocus == null ? undefined : () => onChangeFocus(false),
+            ),
+    }),
+    onPropsChange(['node', 'focused'], props => {
+      const { node, focused, buffer } = props
+      if (node == null || focused == null || focused === buffer) {
+        return
+      }
+      node[focused ? 'focus' : 'blur']()
+    }),
+    mapProps(props => ({ ...withBuffer.omit(props), focused: props.buffer })),
   ),
 )
 
