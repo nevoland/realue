f266d090a3cf88fa889116c202bd52be63ebedd6
David Bonnet <david@bonnet.cc>
2018-03-15 15:51:16 +0100

    fix(string): Add default value to string

--- src/string.js
+++ src/string.js
@@ -1,11 +1,14 @@
-import { branch, withHandlers } from 'recompose'
+import { compose, branch, withHandlers, withProps } from 'recompose'
 
 import { hasProp } from './tools'
 
-export const string = branch(
-  hasProp('onChange'),
-  withHandlers({
-    onChange: ({ name, onChange }) => event =>
-      onChange(event.target.value, name, event),
-  }),
+export const string = compose(
+  branch(({ value }) => value == null, withProps({ value: '' })),
+  branch(
+    hasProp('onChange'),
+    withHandlers({
+      onChange: ({ name, onChange }) => event =>
+        onChange(event.target.value, name, event),
+    }),
+  ),
 )
