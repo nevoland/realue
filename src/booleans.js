e400d07f2e10da338aedcd689c247c57224c024c
David Bonnet <david@bonnet.cc>
2018-05-10 23:44:30 +0200

    fix(booleans): Remove redundant `toggle()`

--- src/booleans.js
+++ src/booleans.js
@@ -1,16 +1,5 @@
-import { compose, branch, withHandlers, withProps } from 'recompose'
+import { branch, withProps } from 'recompose'
 
-import { hasProp, hasNotProp } from './tools'
+import { hasNotProp } from './tools'
 
-export const boolean = compose(
-  branch(hasNotProp('value'), withProps({ value: false })),
-  branch(
-    hasProp('onChange'),
-    compose(
-      withHandlers({
-        toggle: ({ value, name, onChange }) => payload =>
-          onChange(!value, name, payload),
-      }),
-    ),
-  ),
-)
+export const boolean = branch(hasNotProp('value'), withProps({ value: false }))
