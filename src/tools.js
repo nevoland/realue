33918043580d143e6595b396f7402ad0459f6219
David Bonnet <david@bonnet.cc>
2019-01-12 19:33:22 +0100

    feat: expose lazyProperty

--- src/tools.js
+++ src/tools.js
@@ -360,3 +360,15 @@ export function cycledProp(options) {
     },
   })
 }
+
+export function lazyProperty(object, propertyName, valueBuilder) {
+  /*
+  Returns `object[propertyName]` if not `nil`, otherwise sets the result of `valueBuilder(object)` to it and returns it.
+  This enables setting properties only when it is first fetched.
+  */
+  const value = object[propertyName]
+  if (value != null) {
+    return value
+  }
+  return (object[propertyName] = valueBuilder(object))
+}
