2731e2834b462bc3c5c26ad2090c379f96eef38a
David Bonnet <david@bonnet.cc>
2019-02-19 21:49:18 +0100

    feat(properties): add `sideEffect` decorator
    Similar to the `useEffect` hook, enables declaring effect-full handlers
    with a simple cleanup method.

--- src/properties.js
+++ src/properties.js
@@ -43,6 +43,55 @@ export function makeShouldHandle(shouldHandleOrKeys) {
     : (props, nextProps) => !same(props, nextProps, shouldHandleOrKeys)
 }
 
+export function withEffect(shouldHandleOrKeys, handler) {
+  /*
+  Runs `handler(props)` at mount and on update when `shouldHandleOrKeys`, in case it is an array of prop names, mentions a prop name whose value changed, or, in case of a function, returns `true` when called with `(prevProps, nextProps)`.
+  If the handler returns a callback, it is called on update before the `handler` or on unmount.
+
+  Example:
+
+    // Listens for a given event and updates whenever `event` or `listener` changes
+    const withListener = withEffect(
+      ['event', 'listener'],
+      ({ event: eventName, listener }) => {
+        window.addEventListener(eventName, listener)
+        return () => window.removeEventListener(eventName, listener)
+      },
+    )
+  */
+  const shouldHandle = makeShouldHandle(shouldHandleOrKeys)
+  return (Component) =>
+    setWrapperName(
+      Component,
+      class withEffect extends BaseComponent {
+        constructor(props) {
+          super(props)
+          this.cleanup = null
+        }
+        componentDidMount() {
+          // No state update necessary here
+          this.cleanup = handler(this.props)
+        }
+        componentDidUpdate(prevProps, prevState) {
+          if (shouldHandle(prevProps, this.props)) {
+            if (typeof this.cleanup === 'function') {
+              this.cleanup()
+            }
+            this.cleanup = handler(this.props)
+          }
+        }
+        componentWillUnmount() {
+          if (typeof this.cleanup === 'function') {
+            this.cleanup()
+          }
+        }
+        render() {
+          return $(Component, this.props)
+        }
+      },
+    )
+}
+
 export function onPropsChange(
   shouldHandleOrKeys,
   handler,
