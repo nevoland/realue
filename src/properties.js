ff8d8404739fba2e5fda139c92f07597263719f6
David Bonnet <david@bonnet.cc>
2019-03-20 08:59:51 +0100

    feat(properties): enhance logProps
    Now logs props before the element renders, resulting in a natural
    sequence of element prop logs.
    Function props are wrapped with a logger that shows the provided
    parameters at each call.

--- src/properties.js
+++ src/properties.js
@@ -7,11 +7,68 @@ import {
   debounce,
   throttle,
   indexOf,
+  identity,
 } from 'lodash'
 import { mapProps, withHandlers } from 'recompose'
 
 import { EMPTY_OBJECT, same } from './immutables'
 import { $, setWrapperName, getGlobal } from './tools'
+
+/* eslint-disable no-console */
+function wrapProps(
+  title,
+  currentWrappedMethods,
+  props,
+  previousProps,
+  propNames,
+) {
+  let wrappedMethods = currentWrappedMethods
+  let updated = false
+  console.group(title)
+  for (const name of propNames || keys(props)) {
+    const value = props[name]
+    const previousValue = previousProps[name]
+    if (value !== previousValue) {
+      if (!updated) {
+        updated = true
+      }
+      if (typeof value === 'function') {
+        if (wrappedMethods === currentWrappedMethods) {
+          wrappedMethods = { ...currentWrappedMethods }
+        }
+        wrappedMethods[name] = (...parameters) => {
+          console.group(`Calling "${title}.${name}" with parameters:`)
+          for (const parameter of parameters) {
+            console.log(parameter)
+          }
+          console.groupEnd()
+          value(...parameters)
+        }
+      }
+      console.group(name)
+      console.log('Previous:', previousValue)
+      console.log('New:', value)
+      console.groupEnd()
+    }
+  }
+  if (!updated) {
+    console.warn('Element got re-rendered with no property changes')
+  }
+  console.groupEnd()
+  for (const name in wrappedMethods) {
+    if (props[name] == null) {
+      if (wrappedMethods === currentWrappedMethods) {
+        wrappedMethods = { ...currentWrappedMethods }
+      }
+      delete wrappedMethods[name]
+    }
+  }
+  /* eslint-enable no-console */
+  return {
+    wrappedMethods,
+    props,
+  }
+}
 
 export function logProps(propNames, title) {
   /*
@@ -19,20 +76,37 @@ export function logProps(propNames, title) {
   The `title` defaults to the component name.
   If `propNames` is `nil`, logs all props.
   */
+  if (process.env.NODE_ENV === 'production') {
+    return identity
+  }
   return (Component) =>
-    onPropsChange(
-      propNames || undefined,
-      (props) => {
-        /* eslint-disable no-console */
-        console.group(title || Component.displayName || Component.name)
-        for (const name of propNames || keys(props)) {
-          console.log(name, props[name])
+    setWrapperName(
+      Component,
+      class logProps extends BaseComponent {
+        constructor(props) {
+          super(props)
+          this.state = wrapProps(
+            title || Component.displayName || Component.name,
+            EMPTY_OBJECT,
+            props,
+            EMPTY_OBJECT,
+            propNames,
+          )
         }
-        console.groupEnd()
-        /* eslint-enable no-console */
+        static getDerivedStateFromProps(props, state) {
+          return wrapProps(
+            title || Component.displayName || Component.name,
+            state.wrappedMethods,
+            props,
+            state.props,
+            propNames,
+          )
+        }
+        render() {
+          return $(Component, { ...this.props, ...this.state.wrappedMethods })
+        }
       },
-      true,
-    )(Component)
+    )
 }
 
 export function omitProps(propNames) {
