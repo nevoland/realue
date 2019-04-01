0600eeed674d887034923ce708afaae9e3b2ede9
David Bonnet <david@bonnet.cc>
2019-04-01 09:25:53 +0200

    feat(values): optimize transformable
    - Transforms value and onChange in a single component

--- src/values.js
+++ src/values.js
@@ -2,7 +2,7 @@ import { Component as BaseComponent } from 'react'
 import { compose, branch, withHandlers, withPropsOnChange } from 'recompose'
 import { memoize, get } from 'lodash'
 
-import { $, hasProp, hasProps } from './tools'
+import { $, hasProp, hasProps, lazyProperty, setWrapperName } from './tools'
 import {
   delayedProp,
   syncedProp,
@@ -26,46 +26,66 @@ Sets `value` to `initialValue` on first render, if `initialValue` is not `nil`, 
 */
 export const initialValue = initialProp('value')
 
-export const transformable = compose(
+function transformedOnChange(element) {
+  return (value, name, payload) => {
+    const { props } = element
+    return props.onChange(
+      props.transformOnChange(value, name, payload),
+      name,
+      payload,
+    )
+  }
+}
+
+export const transformable = (Component) =>
   /*
   Replaces `value` with the return value of `transformValue(value, previous: { transformedValue?, value? })`, if set. Note that `previous` is not provided when the component first mounts, since there are no previous prop values.
   Replaces `value` passed to `onChange(value, name, payload)` with the return value of `transformOnChange(value, name, payload)`, if set.
   */
-  branch(
-    hasProp('transformValue'),
-    (Component) =>
-      class transformable extends BaseComponent {
-        constructor(props) {
-          super(props)
-          this.state = this.constructor.getDerivedStateFromProps(
-            props,
-            EMPTY_OBJECT,
-          )
+  setWrapperName(
+    Component,
+    class transformable extends BaseComponent {
+      constructor(props) {
+        super(props)
+        const { value, transformValue } = props
+        this.state = {
+          value,
+          transformValue,
+          transformedValue:
+            transformValue && transformValue(value, EMPTY_OBJECT),
         }
-        static getDerivedStateFromProps({ value, transformValue }, state) {
-          return value === state.value && state !== EMPTY_OBJECT
-            ? null
+      }
+      static getDerivedStateFromProps({ value, transformValue }, state) {
+        return value === state.value && transformValue === state.transformValue
+          ? null
+          : {
+              value,
+              transformValue,
+              transformedValue: transformValue && transformValue(value, state),
+            }
+      }
+      render() {
+        const { props, state } = this
+        const { transformValue, transformOnChange } = this
+        return $(
+          Component,
+          !(transformValue || transformOnChange)
+            ? props
             : {
-                transformedValue: transformValue(value, state),
-                value,
-              }
-        }
-        render() {
-          return $(Component, {
-            ...this.props,
-            value: this.state.transformedValue,
-          })
-        }
-      },
-  ),
-  branch(
-    hasProps(['onChange', 'transformOnChange']),
-    withHandlers({
-      onChange: ({ onChange, transformOnChange }) => (value, name, payload) =>
-        onChange(transformOnChange(value, name, payload), name, payload),
-    }),
-  ),
-)
+                ...props,
+                value: transformValue ? state.transformedValue : props.value,
+                onChange: transformOnChange
+                  ? lazyProperty(
+                      this,
+                      'transformedOnChange',
+                      transformedOnChange,
+                    )
+                  : props.onChange,
+              },
+        )
+      }
+    },
+  )
 
 export const filterable = compose(
   /*
