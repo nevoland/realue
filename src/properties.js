cbeb11843d7ff1d5bdd6c284b778ec1ce69b7f80
David Bonnet <david@bonnet.cc>
2020-01-20 18:10:10 +0100

    fix(properties): correctly set prop in defaultProp

--- src/properties.js
+++ src/properties.js
@@ -333,7 +333,7 @@ export function defaultProp(options) {
           ? props
           : {
               ...props,
-              value: value == null ? defaultValue : value,
+              [name]: value == null ? defaultValue : value,
             },
       )
     })
