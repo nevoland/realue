288c0dff563eab41fb95fd47efd38a22a7844aa8
David Bonnet <david@bonnet.cc>
2023-07-17 12:21:52 +0200

    Add logProps

---
+++ lib/tools/logProps.ts
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from "../dependencies";
+
+type Props = { [name: string]: any };
+
+export function logProps(title: string, props: Props) {
+  const propNames = Object.keys(props).sort();
+  const propValues = propNames.map((name) => props[name]);
+  const ref = useRef<Props>();
+  const previousProps = ref.current ?? {};
+  useEffect(() => {
+    console.group(title);
+    console.table(
+      Object.fromEntries(
+        propNames
+          .map(
+            (name) =>
+              [
+                name,
+                {
+                  previous: previousProps[name],
+                  current: props[name],
+                },
+              ] as const,
+          )
+          .filter(([_name, { previous, current }]) => previous !== current),
+      ),
+    );
+    console.groupEnd();
+    ref.current = props;
+  }, propValues);
+}
+
+function formatValue(value: any) {
+  return JSON.stringify(value, null, 2);
+}
