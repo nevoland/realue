caae66936f2aeb292c431974091f2bc9517f2910
David Bonnet <david@bonnet.cc>
2023-11-02 11:41:19 +0100

    Add `trigger` support to `useResilient`

--- lib/hooks/useResilient.ts
+++ lib/hooks/useResilient.ts
@@ -1,14 +1,18 @@
 import { useRef } from "../dependencies";
 
 /**
- * Returns the last non-undefined version of the provided `value`, or `undefined`.
+ * Returns the last non-undefined version of the provided `value` or when `trigger` was truthy, or `undefined`.
  *
  * @param value The `value` to keep a non-undefined value of.
+ * @param trigger An optional `trigger` that sets the last value if it is `truthy`.
  * @returns The last non-undefined version of the provided `value`, or `undefined`.
  */
-export function useResilient<T>(value: T | undefined): T | undefined {
+export function useResilient<T>(
+  value: T | undefined,
+  trigger?: boolean,
+): T | undefined {
   const ref = useRef(value);
-  if (value !== undefined) {
+  if ((value !== undefined && trigger === undefined) || trigger) {
     ref.current = value;
   }
   return ref.current;
