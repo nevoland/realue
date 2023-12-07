2038dc97af05ece2d62c834faeb75af6c6956cee
David Bonnet <david@bonnet.cc>
2023-12-07 14:37:40 +0100

    Fix `useTransform` to play nice with types

--- lib/hooks/useTransform.ts
+++ lib/hooks/useTransform.ts
@@ -1,28 +1,14 @@
 import { useMemo } from "../dependencies.js";
-import type { ErrorReport, Name, NevoProps } from "../types";
+import type {
+  ErrorMutator,
+  ErrorReport,
+  NevoProps,
+  UseTransformOptions,
+  ValueMutator,
+} from "../types";
 
 /**
- * Options for `useTransform`.
- */
-type UseTransformOptions<T, U> = {
-  /**
-   * Transform the incoming `value`.
-   *
-   * @param value The incoming `value` to transform.
-   * @returns The transformed value.
-   */
-  value: (value: T) => U;
-  /**
-   * Transforms the outgoing `value` passed to the `onChange` callback.
-   *
-   * @param value The outgoing `value` to transform.
-   * @returns The transformed value.
-   */
-  onChange: (value: U) => T;
-};
-
-/**
- * Transforms the incoming `value` and the outgoing `value` passed to the `onChange` callback.
+ * Transforms the incoming `value` and the outgoing `value` passed to the `onChange` callback, and optionally the incoming `error` and the outgoing `error` passed to the `onChangeError` callback.
  *
  * @param props The props holding the `value` and `onChange` callbacks.
  * @param options Options for `useTransform`.
@@ -31,16 +17,37 @@ type UseTransformOptions<T, U> = {
 export function useTransform<T, U>(
   props: NevoProps<T>,
   options: UseTransformOptions<T, U>,
-): NevoProps<U, Name, ErrorReport<T>> {
-  const value = useMemo(() => options.value(props.value), [props.value]);
-  const onChange = useMemo(
+): NevoProps<U> {
+  const value = useMemo(
+    () => options.value(props.value),
+    [props.value, options.value],
+  );
+  const onChange: ValueMutator<U> | undefined = useMemo(
     () =>
       props.onChange === undefined
         ? undefined
-        : (value: U, name: Name) => {
+        : (value, name) => {
             props.onChange!(options.onChange(value), name);
           },
-    [props.onChange],
+    [props.onChange, options.onChange],
   );
-  return { ...props, onChange, value };
+  const error = useMemo(
+    () =>
+      options.error === undefined
+        ? (props.error as unknown as ErrorReport<U>)
+        : options.error(props.error),
+    [props.error, options.error],
+  );
+  const onChangeError: ErrorMutator<ErrorReport<U>> | undefined = useMemo(
+    () =>
+      props.onChangeError === undefined
+        ? undefined
+        : options.onChangeError === undefined
+        ? (props.onChangeError as unknown as ErrorMutator<ErrorReport<U>>)
+        : (error, name) => {
+            props.onChangeError!(options.onChangeError!(error), name);
+          },
+    [props.error, options.error],
+  );
+  return { error, name: props.name, onChange, onChangeError, value };
 }
--- lib/types.ts
+++ lib/types.ts
@@ -103,6 +103,46 @@ export type NevoPropsAdapted<
   Property<`onChange${Capitalize<K>}`, ValueMutator<T, N>> &
   Property<`onChange${Capitalize<K>}Error`, ErrorMutator<E, N>>;
 
+/**
+ * Options for `useTransform`.
+ */
+export type UseTransformOptions<T, U> = {
+  /**
+   * Transform the incoming `value`.
+   *
+   * @param value The incoming `value` to transform.
+   * @returns The transformed value.
+   */
+  value: (value: T) => U;
+  /**
+   * Transforms the outgoing `value` passed to the `onChange` callback.
+   *
+   * @param value The outgoing `value` to transform.
+   * @returns The transformed value.
+   */
+  onChange: (value: U) => T;
+} & (
+  | {
+      /**
+       * Optionally transform the incoming `error`.
+       *
+       * @param error The incoming `error` to transform.
+       * @returns The transformed error.
+       */
+      error: (error: ErrorReport<T> | undefined) => ErrorReport<U> | undefined;
+      /**
+       * Optionally transform the outgoing `error` passed to the `onChangeError` callback.
+       *
+       * @param error The outgoing `error` to transform.
+       * @returns The transformed error.
+       */
+      onChangeError: (
+        error: ErrorReport<U> | undefined,
+      ) => ErrorReport<T> | undefined;
+    }
+  | never
+);
+
 export type OptionPropsAdapted<
   T,
   K extends string,
