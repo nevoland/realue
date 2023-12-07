e10a3c8ca7b1ed879d0cb31e16a3233bdcdf3669
David Bonnet <david@bonnet.cc>
2023-12-07 10:25:14 +0100

    Add `NeverNevoProps` type

--- lib/types.ts
+++ lib/types.ts
@@ -81,6 +81,17 @@ export type NevoProps<T, N extends string = Name, E = ErrorReport<T>> = {
   onChangeError?: ErrorMutator<E, N>;
 };
 
+/**
+ * Excludes the props following the NEVO pattern. Useful for creating discriminated union types that enable component uses that do not necessitate the NEVO pattern.
+ */
+export type NeverNevoProps = {
+  value?: never;
+  name?: never;
+  onChange?: never;
+  error?: never;
+  onChangeError?: never;
+};
+
 export type NevoPropsAdapted<
   T,
   K extends string,
