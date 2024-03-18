de05f26ba3a770904a1081942404c0a4e607b93b
David Bonnet <david@bonnet.cc>
2024-03-18 17:10:03 +0100

    fix(types): cannot override item error

--- lib/types/ItemProps.ts
+++ lib/types/ItemProps.ts
@@ -1,7 +1,10 @@
 import type { ErrorReport } from "./ErrorReport";
 import type { NevoProps } from "./NevoProps";
 
-export type ItemProps<T> = NevoProps<T, ErrorReport<T>> & {
+export type ItemProps<T, E extends ErrorReport<T> = ErrorReport<T>> = NevoProps<
+  T,
+  E
+> & {
   key: string;
   id: string;
 };
