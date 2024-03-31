6394f829bbebbecb521d79e58ebad7d4a0b560bf
David Bonnet <david@bonnet.cc>
2024-03-31 13:06:54 +0200

    fix(tool): loosen up type

--- lib/tools/withoutNevoProps.ts
+++ lib/tools/withoutNevoProps.ts
@@ -1,4 +1,4 @@
-import type { NevoName, NevoProps } from "../types";
+import type { NevoName } from "../types";
 
 /**
  * Returns the properties without the ones according to the Nevo pattern.
@@ -6,9 +6,10 @@ import type { NevoName, NevoProps } from "../types";
  * @param props Properties according to the Nevo pattern with extra properties.
  * @returns The propterties without the ones according to the Nevo pattern.
  */
-export function withoutNevoProps<T, P extends NevoProps<T>>(
+export function withoutNevoProps<P extends object>(
   props: P,
 ): Omit<P, NevoName> {
-  const { name, error, value, onChange, onChangeError, ...otherProps } = props;
+  const { name, error, value, onChange, onChangeError, ...otherProps } =
+    props as any;
   return otherProps;
 }
