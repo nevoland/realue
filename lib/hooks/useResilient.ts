bb4e6975db46f020808f504bfd727e4e604ac592
David Bonnet <david@bonnet.cc>
2024-08-06 10:32:41 +0200

    chore: update `useResilient` doc

--- lib/hooks/useResilient.ts
+++ lib/hooks/useResilient.ts
@@ -4,9 +4,9 @@ import { useRef } from "../dependencies.js";
  * Returns the provided `value` when `trigger` was truthy.
  * By default, `trigger` is `true` when `value` is not `undefined`.
  *
- * @param value The `value` to keep a non-undefined value of.
+ * @param value The `value` to make resilient.
  * @param trigger The `trigger` that sets the last value if it is `truthy`.
- * @returns The last non-undefined version of the provided `value`, or `undefined`.
+ * @returns The last `value` when `trigger` was `truthy`.
  */
 export function useResilient<T>(
   value: T,
