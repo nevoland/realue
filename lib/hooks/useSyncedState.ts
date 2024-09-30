8a6a0e0e2cd5cbfd6cdb8d7ce380fc07ff18b38d
David Bonnet <david@bonnet.cc>
2024-09-30 09:40:14 +0200

    fix(useSyncedState): discard NEVO props
    BREAKING CHANGE: `useSyncedState` now takes two arguments, the parent
    state and the parent state updater, instead of NEVO props.

--- lib/hooks/useSyncedState.ts
+++ lib/hooks/useSyncedState.ts
@@ -1,25 +1,45 @@
-import { useCallback, useEffect, useState } from "../dependencies.js";
-import type { NevoProps } from "../types";
+import {
+  type Dispatch,
+  type StateUpdater,
+  useCallback,
+  useMemo,
+  useRef,
+} from "../dependencies.js";
+import type { Name } from "../types.js";
+
+import { useRefresh } from "./useRefresh.js";
 
 /**
- * Creates a state that is synced with its parent.
- * If `props.value` changes, the internal `state` is also updated.
- * Calls to `onChangeState(value)` trigger a call of `props.onChange(state, props.name)`.
+ * Creates a state that is synced with its parent state.
+ * If the provided `state` changes, the returned `state` is set to that provided state.
+ * Calls to the returned `setState(state)` also triggers a call to the optionally provided `setState(state)`.
  *
- * @param props Properties according to the NEVO pattern.
- * @returns The `[state, onChangeState]` tuple.
+ * @param state The provided parent state.
+ * @param setState The optional parent state udpater.
+ * @returns The `[state, setState]` tuple.
  */
+export function useSyncedState<T>(value: T): [T, Dispatch<StateUpdater<T>>];
 export function useSyncedState<T>(
-  props: NevoProps<T>,
-): [T, (value: T) => void] {
-  const [state, onChangeState] = useState<T>(props.value);
-  useEffect(() => onChangeState(props.value), [props.value]);
-  const onChange = useCallback(
-    (value: T) => {
-      onChangeState(value);
-      props.onChange?.(value, props.name);
+  value: T,
+  setValue: Dispatch<StateUpdater<T>>,
+): [T, Dispatch<StateUpdater<T>>];
+export function useSyncedState<T>(
+  value: T,
+  setValue?: Dispatch<StateUpdater<T>>,
+): [T, (value: T, name?: Name) => void] {
+  const onRefresh = useRefresh();
+  const state = useRef(value);
+  useMemo(() => {
+    state.current = value;
+  }, [value]);
+  const onChangeState = useCallback(
+    (value: StateUpdater<T>) => {
+      state.current =
+        typeof value === "function" ? (value as any)(state.current) : value;
+      onRefresh();
+      setValue?.(state.current);
     },
-    [props.onChange],
+    [setValue],
   );
-  return [state, onChange];
+  return [state.current, onChangeState];
 }
