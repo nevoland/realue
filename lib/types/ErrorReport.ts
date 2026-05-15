32305dffec4a1ce09134853b6c3f341b3a6889be
David Bonnet <david@bonnet.cc>
2026-05-15 18:04:27 +0200

    chore: add fixme

--- lib/types/ErrorReport.ts
+++ lib/types/ErrorReport.ts
@@ -2,6 +2,7 @@ import type { ErrorReportArray } from "./ErrorReportArray";
 import type { ErrorReportObject } from "./ErrorReportObject";
 import type { ErrorReportValue } from "./ErrorReportValue";
 
+// FIXME: Simplify the error report type
 export type ErrorReport<T, U = NonNullable<T>> = [U] extends [
   readonly unknown[],
 ]
