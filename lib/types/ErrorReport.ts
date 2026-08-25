83709f9838ee56af94dc6598a0bb03579ef097ee
David Bonnet <david@bonnet.cc>
2026-08-25 10:33:55 +0200

    fix(ErrorReport): loosen type when `T` is `any`

--- lib/types/ErrorReport.ts
+++ lib/types/ErrorReport.ts
@@ -2,10 +2,12 @@ import type { ErrorReportArray } from "./ErrorReportArray";
 import type { ErrorReportObject } from "./ErrorReportObject";
 import type { ErrorReportValue } from "./ErrorReportValue";
 
-export type ErrorReport<T> = [unknown] extends [T]
-  ? unknown
-  : [NonNullable<T>] extends [readonly unknown[]]
-    ? ErrorReportArray<NonNullable<T>>
-    : [NonNullable<T>] extends [object]
-      ? ErrorReportObject<NonNullable<T>>
-      : ErrorReportValue;
+export type ErrorReport<T> = [0] extends [1 & T]
+  ? any
+  : [unknown] extends [T]
+    ? unknown
+    : [NonNullable<T>] extends [readonly unknown[]]
+      ? ErrorReportArray<NonNullable<T>>
+      : [NonNullable<T>] extends [object]
+        ? ErrorReportObject<NonNullable<T>>
+        : ErrorReportValue;
